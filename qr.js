83d3682c323fee8568cc700f81add6847f80c23a
MR.TIMA <98197960+TEAM-KING-BOT@users.noreply.github.com>
2022-07-01 08:06:45 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -10,25 +10,25 @@ const fs = require('fs');
 async function whatsAsena() {
   const conn = new WAConnection();
   conn.logger.level = 'warn';
-  conn.version = [2, 2126, 14]
+  conn.version = [3,3234, 9]
 
   conn.on('connecting', async () => {
-    console.log(`${chalk.green.bold('T-REX')}${chalk.green.bold('Team')}
-${chalk.white.italic('T-REX String code recipient')}
-${chalk.blue.bold('ℹ️  Connecting T-REX... Please wait.')}`);
+    console.log(`${chalk.green.bold('function/20px')}${chalk.green.bold('replit.js-start')}
+${chalk.white.italic('TEAM-KING-BOT')}
+${chalk.blue.bold('ℹ️  Loading Qr... Please wait.')}`);
   });
 
   conn.on('open', async () => {
     console.log(
-      chalk.green.bold('T-REX QR Code: '),
-      'TREX;;;' +
+      chalk.green.bold('King-Bot QR Code: '),
+      'KINGBOT:::' +
       Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
         'base64'
       )
     );
     await conn.sendMessage(
       conn.user.jid,
-      'TREX;;;' +
+      'KINGBOT:::' +
       Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
         'base64'
       ),
@@ -37,19 +37,19 @@ ${chalk.blue.bold('ℹ️  Connecting T-REX... Please wait.')}`);
     if (conn.user.jid.startsWith('91')) {
       await conn.sendMessage(
         conn.user.jid,
-        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*▪️ T-REX Scanned✅️*\n*▪️Thanks For Choosing T-REX 😈*',
+        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ King-Bot Successfully Scanned✅️*\n*▪️Thanks For Choosing King-Bot 😻💫*',
         MessageType.text
       );
     } else {
       await conn.sendMessage(
         conn.user.jid,
-        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ T-REX Successfully Scanned✅️*\n*▪️Thanks For Choosing T-REX😈*',
+        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ King-Bot Successfully Scanned✅️*\n*▪️Thanks For Choosing King-Bot 😻💫*',
         MessageType.text
       );
     }
     console.log(
       chalk.green.bold(
-        "\n\n Nigalkku sandesham pakarthan \n kaliyunnillegil,whatsapp parishodikkuka \n nigalude numberillekku  code ayachinnu!\n\n\n\n"
+        "methanin session eka ganna bari nm qr eka scan karapu whatsapp eke oyage num eken msg ekak avilla athi..eke balanna\n"
       ),
       chalk.green.bold(
         'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
